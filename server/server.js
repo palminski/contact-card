1e0f79101b29bebb5f07aafd6e157f6ee1b0621c
palminski <williambolls98@gmail.com>
2022-11-06 09:55:00 -0800

    added concurrently

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();
+require('./routes/htmlRoutes.js')(app);
+const PORT = process.env.PORT || 3001;
+
+//ASK ABT THESE
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('../client/'));
+
+app.listen(PORT, function() {
+    console.log(`
+---------------------------------------
+    Now Listening on port ${PORT}
+---------------------------------------`)
+})
